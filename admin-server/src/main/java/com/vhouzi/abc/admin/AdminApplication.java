be157ba7479764bfbfb584565f261e65429c4cd2
朱永生 <zhuyongsheng@zhuyongshengdeMacBook-Pro-2018.local>
2020-05-03 18:12:09 +0800

    移除admin-system等module

--- admin-server/src/main/java/com/vhouzi/abc/admin/AdminApplication.java
+++ admin-server/src/main/java/com/vhouzi/abc/admin/AdminApplication.java
@@ -1,6 +1,5 @@
 package com.vhouzi.abc.admin;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -11,14 +10,13 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * 启动程序
  * @author yiran
  */
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class  })
-@MapperScan({"com.vhouzi.abc.admin.*.mapper"})
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class})
 @EnableDiscoveryClient
 public class AdminApplication
 {
     public static void main(String[] args)
     {
         SpringApplication.run(AdminApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  一元ABC管理平台启动成功   ლ(´ڡ`ლ)");
+        System.out.println("(♥◠‿◠)ﾉﾞ  1元ABC管理平台启动成功   ლ(´ڡ`ლ)");
     }
 }
